a6ba495f75d6ad3c7e3d8924eb9daf76009d05f2
Steve Thomas <stevethomas01@yahoo.com>
2024-10-31 12:49:16 -0500

    Add PLSS databases to the S3 bucket

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -8,3 +8,24 @@ resource "aws_lightsail_bucket" "bucket" {
 resource "aws_lightsail_bucket_access_key" "bucket-access-key" {
   bucket_name = aws_lightsail_bucket.bucket.name
 }
+
+resource "null_resource" "upload_env_to_s3" {
+  provisioner "local-exec" {
+    command = "AWS_DEFAULT_REGION=us-east-1; AWS_ACCESS_KEY_ID=${aws_lightsail_bucket_access_key.offset-well-identification-lightsail-bucket-access-key.access_key_id}; AWS_SECRET_ACCESS_KEY=${aws_lightsail_bucket_access_key.offset-well-identification-lightsail-bucket-access-key.secret_access_key}; aws s3 cp new_mexico_land_survey_system.db s3://${aws_lightsail_bucket.offset-well-identification-lightsail-bucket.name}/new_mexico_land_survey_system.db;"
+  }
+
+  # Ensure the S3 bucket is created before the upload
+  depends_on = [ aws_lightsail_bucket.bucket,
+                 aws_lightsail_bucket_access_key.bucket-access-key ]
+}
+
+resource "null_resource" "upload_env_to_s3" {
+  provisioner "local-exec" {
+    command = "AWS_DEFAULT_REGION=us-east-1; AWS_ACCESS_KEY_ID=${aws_lightsail_bucket_access_key.offset-well-identification-lightsail-bucket-access-key.access_key_id}; AWS_SECRET_ACCESS_KEY=${aws_lightsail_bucket_access_key.offset-well-identification-lightsail-bucket-access-key.secret_access_key}; aws s3 cp texas_land_survey_system.db s3://${aws_lightsail_bucket.offset-well-identification-lightsail-bucket.name}/texas_land_survey_system.db;"
+  }
+
+  # Ensure the S3 bucket is created before the upload
+  depends_on = [ aws_lightsail_bucket.bucket,
+                 aws_lightsail_bucket_access_key.bucket-access-key ]
+}
+
