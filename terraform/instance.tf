cc00d1bbb0f40976a9dd735126848249458938b5
Steve Thomas <stevethomas01@yahoo.com>
2024-10-31 18:57:34 -0500

    Debug

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -159,6 +159,7 @@ resource "aws_lightsail_instance" "instance" {
         sudo touch /etc/nginx/.htpasswd
         sudo chmod 644 /etc/nginx/.htpasswd
         sudo chown www-data:www-data /etc/nginx/.htpasswd
+        export USERNAME="afe-admin"
         echo "USERNAME is $USERNAME"
         echo "APP_SECRET is $APP_SECRET"
         echo $APP_SECRET | sudo htpasswd -ci /etc/nginx/.htpasswd $USERNAME
