245c716410299e4c08bf92e3cafa94b38b7e8ca9
Steve Thomas <stevethomas01@yahoo.com>
2025-04-16 10:20:57 -0500

    Fix key pair name reference in Lightsail instance configuration

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,7 +1,7 @@
 # Create a new Lightsail Key Pair
-resource "aws_lightsail_key_pair" "key-pair" {
-  name = "${var.app}-key-pair"
-}
+# resource "aws_lightsail_key_pair" "key-pair" {
+#   name = "${var.app}-key-pair"
+# }
 
 # Lightsail Instance
 resource "aws_lightsail_instance" "instance" {
@@ -9,7 +9,7 @@ resource "aws_lightsail_instance" "instance" {
     availability_zone   = "us-east-1a"
     blueprint_id        = "ubuntu_24_04"
     bundle_id           = "medium_3_0" 
-    key_pair_name       = aws_lightsail_key_pair.key-pair.name
+    key_pair_name       = "${var.app}-key-pair"
     user_data = <<-EOF
         #!/bin/bash
         apt-get update -y
